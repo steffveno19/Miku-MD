37661cd61f16df1dd732d47712f140d817420e66
steffveno19 <118614594+steffveno19@users.noreply.github.com>
2022-11-19 22:56:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,20 +7,20 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.Owner = ['917561808862']
+global.OwnerNumber = ['917561808862']
+global.ownertag = ['917561808862']
+global.BotName = "Steffveno"
+global.packname = "Steff veno"
+global.author = "By: Steffveno"
+global.OwnerName = "Steffveno"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
 global.sessionName = "session"
 
 
 
-global.prefa = ['-'] // Prefix ---  change it to anythong you want except @//
+global.prefa = ['.'] // Prefix ---  change it to anythong you want except @//
 
 
 global.location = "West Bengal, India"
